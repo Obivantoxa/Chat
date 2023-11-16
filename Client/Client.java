4b2b3bbe653428c4564786863b43d3d38b19b2a6
Obivantoxa <Obivan.toxa@yandex.ru>
2023-11-16 22:13:40 +0300

    chat #2

---
+++ Client/Client.java
@@ -0,0 +1,32 @@
+package Chat.Client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost",9445);
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream is = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+            while (true){
+                String message = scanner.nextLine();
+                dos.writeUTF(message);
+                String response = is.readUTF();
+                System.out.println(response);
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
--- Server/Server.java
+++ Server/Server.java
@@ -1,7 +1,11 @@
 package Chat.Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Locale;
 
 public class Server {
 
@@ -10,8 +14,32 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(9445);
             System.out.println("Сервер запущен");
-            serverSocket.accept();// ждет пока к нему подключится кто-то
-            System.out.println("Клиент подключился");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();// ждет пока к нему подключится кто-то
+                System.out.println("Клиент подключился");
+
+                // второй поток с сообщениями
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+                            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+                            while (true){
+                                String message = inputStream.readUTF();
+                                outputStream.writeUTF(message.toUpperCase(Locale.ROOT));
+                                System.out.println(message);
+                            }
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                });
+                thread.start();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
